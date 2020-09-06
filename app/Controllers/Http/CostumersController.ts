03a2129799f31ec963df2e023531dc9dda55bb58
ipdmartins <ipdmartins@gmail.com>
2020-09-06 11:52:35 -0300

    refactor: fixing some code formatting

--- app/Controllers/Http/CostumersController.ts
+++ app/Controllers/Http/CostumersController.ts
@@ -1,21 +1,21 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { schema } from '@ioc:Adonis/Core/Validator'
-import Costumer from 'App/Models/Costumer'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { schema } from "@ioc:Adonis/Core/Validator";
+import Costumer from "App/Models/Costumer";
 
 export default class CostumersController {
-  public async index ({ view }: HttpContextContract) {
-    const costumers = await Costumer.all()
+  public async index({ view }: HttpContextContract) {
+    const costumers = await Costumer.all();
 
-    return view.render('costumer/index', {
+    return view.render("costumer/index", {
       costumers,
-    })
+    });
   }
 
-  public async create ({ view }: HttpContextContract) {
-    return view.render('costumer/create')
+  public async create({ view }: HttpContextContract) {
+    return view.render("costumer/create");
   }
 
-  public async store ({ request, session, response }: HttpContextContract) {
+  public async store({ request, session, response }: HttpContextContract) {
     const costumerSchema = schema.create({
       name: schema.string(),
       cpf: schema.string(),
@@ -25,46 +25,46 @@ export default class CostumersController {
       city: schema.string(),
       zip: schema.string(),
       state: schema.string(),
-    })
+    });
 
     const data = await request.validate({
       schema: costumerSchema,
       messages: {
-        'name.required': 'Please enter the costumer name',
-        'CPF.required': 'Please enter the costumer CPF',
-        'street.required': 'Please enter the costumer street',
-        'number.required': 'Please enter the costumer number',
-        'district.required': 'Please enter the costumer district',
-        'city.required': 'Please enter the costumer city',
-        'zip.required': 'Please enter the costumer zip',
-        'state.required': 'Please enter the costumer state',
+        "name.required": "Please enter the costumer name",
+        "cpf.required": "Please enter the costumer CPF",
+        "street.required": "Please enter the costumer street",
+        "number.required": "Please enter the costumer number",
+        "district.required": "Please enter the costumer district",
+        "city.required": "Please enter the costumer city",
+        "zip.required": "Please enter the costumer zip",
+        "state.required": "Please enter the costumer state",
       },
       cacheKey: request.url(),
-    })
+    });
 
-    await Costumer.create(data)
+    await Costumer.create(data);
 
-    session.flash('success', 'Costumer created successfully')
-    response.redirect('back')
+    session.flash("success", "Costumer created successfully");
+    response.redirect("back");
   }
 
-  public async show ({ params: { id }, view }: HttpContextContract) {
-    const costumer = await Costumer.findOrFail(id)
+  public async show({ params: { id }, view }: HttpContextContract) {
+    const costumer = await Costumer.findOrFail(id);
 
-    return view.render('costumer/show', {
+    return view.render("costumer/show", {
       costumer,
-    })
+    });
   }
 
-  public async edit ({ params: { id }, view }: HttpContextContract) {
-    const costumer = await Costumer.findOrFail(id)
+  public async edit({ params: { id }, view }: HttpContextContract) {
+    const costumer = await Costumer.findOrFail(id);
 
-    return view.render('costumer/edit', {
+    return view.render("costumer/edit", {
       costumer,
-    })
+    });
   }
 
-  public async update ({
+  public async update({
     request,
     session,
     response,
@@ -79,43 +79,43 @@ export default class CostumersController {
       city: schema.string(),
       zip: schema.string(),
       state: schema.string(),
-    })
+    });
 
     const data = await request.validate({
       schema: costumerSchema,
       messages: {
-        'name.required': 'Please enter the costumer name',
-        'CPF.required': 'Please enter the costumer CPF',
-        'street.required': 'Please enter the costumer street',
-        'number.required': 'Please enter the costumer number',
-        'district.required': 'Please enter the costumer district',
-        'city.required': 'Please enter the costumer city',
-        'zip.required': 'Please enter the costumer zip',
-        'state.required': 'Please enter the costumer state',
+        "name.required": "Please enter the costumer name",
+        "cpf.required": "Please enter the costumer CPF",
+        "street.required": "Please enter the costumer street",
+        "number.required": "Please enter the costumer number",
+        "district.required": "Please enter the costumer district",
+        "city.required": "Please enter the costumer city",
+        "zip.required": "Please enter the costumer zip",
+        "state.required": "Please enter the costumer state",
       },
       cacheKey: request.url(),
-    })
+    });
 
-    const costumer = await Costumer.findOrFail(id)
+    const costumer = await Costumer.findOrFail(id);
 
-    costumer.merge(data)
+    costumer.merge(data);
 
-    await costumer.save()
+    await costumer.save();
 
-    await session.flash('success', 'Costumer updated successfully')
-    response.redirect('back')
+    await session.flash("success", "Costumer updated successfully");
+    response.redirect("back");
   }
 
-  public async destroy ({
+  public async destroy({
     params: { id },
     session,
     response,
   }: HttpContextContract) {
-    const costumer = await Costumer.findOrFail(id)
+    const costumer = await Costumer.findOrFail(id);
 
-    await costumer.delete()
+    await costumer.delete();
 
-    await session.flash('success', 'Costumer deleted successfully')
-    response.redirect('back')
+    await session.flash("success", "Costumer deleted successfully");
+    response.redirect("back");
   }
 }
